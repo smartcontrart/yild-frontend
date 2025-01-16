34da04b6bf853107dbbf54e49889dda11bc30309
disco-quotidie <disco.quotidie@outlook.com>
2025-01-16 12:40:17 -0800

    feat: add collect Fees function

--- src/pages/positions/[id]/index.tsx
+++ src/pages/positions/[id]/index.tsx
@@ -13,10 +13,14 @@ import {
   X,
 } from "lucide-react";
 import { useParams } from "next/navigation";
-import { useAccount } from "wagmi";
+import { useAccount, useWriteContract } from "wagmi";
+import { DEPLOYED_ADDRESS } from "@/constant";
+import Abi from "@/abi/PositionManager.json";
 
 export default function PositionPage() {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const { writeContract } = useWriteContract()
+
   const router = useRouter()
   const positionId = router.query.id;
 
@@ -29,6 +33,18 @@ export default function PositionPage() {
         </p>
       </div>
     );
+  }
+
+  const collectFees = () => {
+    writeContract({
+      abi: Abi.abi,
+      address: `0x${DEPLOYED_ADDRESS.base}`,
+      functionName: 'collectFees',
+      args: [
+        positionId,
+        address
+      ],
+    })
   }
 
   return (
@@ -92,15 +108,15 @@ export default function PositionPage() {
 
         <TabsContent value="manage" className="space-y-4">
           <div className="grid gap-4 md:grid-cols-2">
-            <Button className="w-full" onClick={() => {}}>
+            <Button className="w-full" onClick={() => { }}>
               <PlusCircle className="mr-2 h-4 w-4" />
               Increase Position
             </Button>
-            <Button className="w-full" variant="secondary" onClick={() => {}}>
+            <Button className="w-full" variant="secondary" onClick={() => { }}>
               <MinusCircle className="mr-2 h-4 w-4" />
               Decrease Position
             </Button>
-            <Button className="w-full" variant="destructive" onClick={() => {}}>
+            <Button className="w-full" variant="destructive" onClick={() => { }}>
               <X className="mr-2 h-4 w-4" />
               Close Position
             </Button>
@@ -109,11 +125,11 @@ export default function PositionPage() {
 
         <TabsContent value="fees" className="space-y-4">
           <div className="grid gap-4 md:grid-cols-2">
-            <Button className="w-full" onClick={() => {}}>
+            <Button className="w-full" onClick={collectFees}>
               <Coins className="mr-2 h-4 w-4" />
               Collect Fees
             </Button>
-            <Button className="w-full" onClick={() => {}}>
+            <Button className="w-full" onClick={() => { }}>
               <RefreshCw className="mr-2 h-4 w-4" />
               Compound Fees
             </Button>
