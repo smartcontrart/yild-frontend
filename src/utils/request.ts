6e5fa329c8e1ebea8d95f8d224b63d7ad40a2e16
disco-quotidie <disco.quotidie@outlook.com>
2025-01-18 04:15:56 -0800

    feat: add utils folder

---
+++ src/utils/request.ts
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+export const getParaswapData = async (srcToken: string, destToken: string, amount: number, slippage: number) => {
+    // const url = 'https://api.paraswap.io/transactions/8453';
+    // const data = {
+    //     srcToken,
+    //     destToken,
+    //     amount,
+    //     slippage
+    // };
+
+    // try {
+    //     const response = await axios.post(url, data);
+    //     console.log('Response:', response.data);
+    // } catch (error) {
+    //     console.error('Error:', error);
+    // }
+    
+    return null
+}
