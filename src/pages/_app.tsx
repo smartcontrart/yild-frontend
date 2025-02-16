9ab0969474ba197279471a8317179119faa75795
disco-quotidie <disco.quotidie@outlook.com>
2025-02-16 02:26:11 -0800

    feat: add shadcn skeleton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,25 +5,33 @@ import { Providers } from '@/components/providers';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <div className={`min-h-screen bg-background ${inter.className}`}>
-        <header className="border-b">
-          <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-            <Link href="/" className="hover:cursor-pointer">
-              <h1 className="text-xl font-bold">Yild Finance</h1>
-            </Link>
-            <ConnectButton />
-          </nav>
-        </header>
-        <main className="container mx-auto px-4 py-8">
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </Providers>
+    <>
+      <Head>
+        <title>Yild Finance</title>
+        <meta name="description" content="Yild Finance is an advanced DeFi platform that automates Uniswap V3 liquidity provision, optimizing yield strategies for effortless passive income. Maximize your earnings with automated LP management." />
+        <meta property="og:image" content="/y.png" />
+      </Head>
+      <Providers>
+        <div className={`min-h-screen bg-background ${inter.className}`}>
+          <header className="border-b">
+            <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+              <Link href="/" className="hover:cursor-pointer">
+                <h1 className="text-xl font-bold">Yild Finance</h1>
+              </Link>
+              <ConnectButton label='Sign In' />
+            </nav>
+          </header>
+          <main className="container mx-auto px-4 py-8">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </Providers>
+    </>
   );
 }
