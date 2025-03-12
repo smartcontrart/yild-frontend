2be13de3f20d7d83b9c3eca4d12466ab3ea8e4a0
disco-quotidie <disco.quotidie@outlook.com>
2025-03-12 14:42:49 -0700

    chore: yild-loading enhance

--- src/components/global/yild-loading.tsx
+++ src/components/global/yild-loading.tsx
@@ -1,8 +1,11 @@
+import { useTheme } from "next-themes"
+
 export function YildLoading({ loading }: { loading: boolean}) {
+  const { theme } = useTheme()
   if (loading)
     return (
-      <div className="flex items-center justify-center h-screen absolute left-0 top-0 w-full bg-white/50 dark:bg-black/50 backdrop-blur">
-        <p className="text-xl animate-pulse text-black">Loading...</p>
+      <div className="flex items-center justify-center h-screen absolute left-0 top-0 w-full bg-white/50 dark:bg-black/50 backdrop-blur z-90">
+        <p className={`text-xl animate-pulse ${theme === "light" ? "text-black" : "white"}`}>YILD IS LOADING...</p>
       </div>
     )
   return <></>
