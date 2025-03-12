da2362b189ca3b6b8cf0e36b98bb57885344f688
disco-quotidie <disco.quotidie@outlook.com>
2025-03-12 14:50:36 -0700

    chore: make yild-loading more blur

--- src/components/global/yild-loading.tsx
+++ src/components/global/yild-loading.tsx
@@ -4,7 +4,7 @@ export function YildLoading({ loading }: { loading: boolean}) {
   const { theme } = useTheme()
   if (loading)
     return (
-      <div className="flex items-center justify-center h-screen absolute left-0 top-0 w-full bg-white/50 dark:bg-black/50 backdrop-blur z-90">
+      <div className="flex items-center justify-center h-screen absolute left-0 top-0 w-full bg-white/90 dark:bg-black/90 backdrop-blur z-90">
         <p className={`text-xl animate-pulse ${theme === "light" ? "text-black" : "white"}`}>YILD IS LOADING...</p>
       </div>
     )
